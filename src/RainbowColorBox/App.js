f3d12478fde285287c5b745fbcc322a7731f8c8f
Dongjin <dongqui9@gmail.com>
2024-07-30 18:37:48 +0900

    Step 1: 상자 색 변경

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 import ColorBox from "./ColorBox";
 
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("");
+
+  const handleInputChange = (e) => setColor(e.currentTarget.value);
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onChange={handleInputChange} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
