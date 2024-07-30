168ce0881601eb25fa829c45159b3dfb2cd4309e
Dongjin <dongqui9@gmail.com>
2024-07-30 18:47:23 +0900

    Step 2: 상자 초기화

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,21 +1,24 @@
 import { useState } from "react";
 
 import ColorBox from "./ColorBox";
+import ResetButton from "./ResetButton";
 
 import "./App.css";
 
 function App() {
   const [color, setColor] = useState("");
 
-  const handleInputChange = (e) => setColor(e.currentTarget.value);
+  const handleChangeInput = (e) => setColor(e.currentTarget.value);
+  const handleClickReset = () => setColor("");
 
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input onChange={handleInputChange} />
+        <input onChange={handleChangeInput} />
       </div>
       <ColorBox color={color} />
+      <ResetButton onClick={handleClickReset} />
     </div>
   );
 }
