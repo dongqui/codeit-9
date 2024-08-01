8c9a61a4fa5670d0b92a97605151a71649c28c7e
KimChungO <kco796@naver.com>
2024-08-01 16:08:15 +0900

    [Add] 초기화 버튼 추가

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,6 +1,7 @@
 import ColorBox from "./ColorBox";
 import "./App.css";
 import { useState } from "react";
+import ResetButton from "./ResetButton";
 
 function App() {
   const [color, setColor] = useState("");
@@ -8,6 +9,11 @@ function App() {
   const handleOnChange = (e) => {
     setColor(e.target.value);
   };
+
+  const handleClear = () => {
+    setColor("");
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
@@ -15,6 +21,7 @@ function App() {
         <input type="text" value={color} onChange={handleOnChange} />
       </div>
       <ColorBox color={color} />
+      <ResetButton onClick={handleClear} />
     </div>
   );
 }
---
+++ src/ResetButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResetButton = ({ onClick }) => {
+  return <button onClick={onClick}>초기화</button>;
+};
+
+export default ResetButton;
