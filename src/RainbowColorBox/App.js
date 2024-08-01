40e18610cceb42c9e96768b2554b9ab1859135e5
ckx9798 <ckx12@Naver.com>
2024-08-01 16:00:11 +0900

    color

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,13 +1,15 @@
 import ColorBox from "./ColorBox";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [color, setColor] = useState('')
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input style={ {backgroundColor : { setColor }}}/>
       </div>
       <ColorBox />
     </div>
