62db64ded48e955ba974db78741c2b1a67a78d7d
junjeeong <wn8624@naver.com>
2024-08-01 14:30:30 +0900

    정준영

--- src/RainbowColorBox/ColorBox.jsx
+++ src/RainbowColorBox/ColorBox.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 export default function ColorBox({ color }) {
+  const [otherColor, setOtherColor] = useState("white");
+
   return (
     <div
       style={{
-        backgroundColor: color,
+        backgroundColor: setOtherColor(color),
         height: "200px",
         width: "200px",
         border: "2px dashed black",
