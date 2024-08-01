e7d23d77c0c1d95d9fdbff17faad2a7f292cd174
cccwon2 <cccwon2@naver.com>
2024-08-01 14:48:49 +0900

    테스트2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import { useRef } from "react";
 import ColorBox from "./ColorBox";
-
 import "./App.css";
-
-let color = "";
-const handleEnter = (e) => {
-  if (e.key === "Enter") {
-    color = this.target.value;
-  }
-};
 
 // color (red, #ff0000 2가지)
 // 상태값 정의, 컬러 박스 변경
 function App() {
+  const textRef = useRef();
+  let color = "";
+
+  const handleKeyDown = (e) => {
+    color = textRef.current.value.trim();
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input type="text" onKeyDown={handleEnter} />
+        <input type="text" ref={textRef} onKeyDown={handleKeyDown} />
       </div>
       <ColorBox props={color} />
     </div>
