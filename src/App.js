5accd3c63663b79e7288292dd384f917a14bc185
cccwon2 <cccwon2@naver.com>
2024-08-01 14:35:11 +0900

    작업중1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import ColorBox from "./ColorBox";
 
 import "./App.css";
 
+let color = "";
+const handleEnter = (e) => {
+  if (e.key === "Enter") {
+    color = this.target.value;
+  }
+};
+
+// color (red, #ff0000 2가지)
+// 상태값 정의, 컬러 박스 변경
 function App() {
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input type="text" onKeyDown={handleEnter} />
       </div>
-      <ColorBox />
+      <ColorBox props={color} />
     </div>
   );
 }
