b2aca58fcf212ec03e505262e9c58861dc0462d2
김예지 <t47047877@gmail.com>
2024-08-01 14:35:48 +0900

    🐛 입력된 color에 따라 배경 바뀌는 기능

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import ColorBox from "./ColorBox";
-
-import "./App.css";
+import ColorBox from './ColorBox';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // 상태값 정의 -> 상태값 변경 -> 컬러박스 변화
+  const [color, setColor] = useState('');
+
+  // 상태값 변경 함수
+  const onChange = (e) => {
+    setColor(e.target.value);
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onChange={onChange} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
