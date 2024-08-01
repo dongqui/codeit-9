c11e2d854eaa6e11da2df4091689b2aabde30b20
김예지 <t47047877@gmail.com>
2024-08-01 15:19:45 +0900

    💄 state 기본값 white로 변경

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState('white');
 
   // 상태값 변경 함수
   const onChange = (e) => {
