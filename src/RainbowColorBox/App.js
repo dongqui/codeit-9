cea75f83266f558ffe58327b33c39b49a6c830da
KimChungO <kco796@naver.com>
2024-08-01 15:49:15 +0900

    [Add] input onChange 기능 추가

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,20 @@
 import ColorBox from "./ColorBox";
-
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
+
+  const handleOnChange = (e) => {
+    setColor(e.target.value);
+  };
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input type="text" value={color} onChange={handleOnChange} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
