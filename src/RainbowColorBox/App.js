ba027c4c301c190c64c68d98fb8c53f69fc8a0a2
mungyun <mungyun1234@naver.com>
2024-08-01 15:57:51 +0900

    Add handleChange

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,21 @@
 import ColorBox from "./ColorBox";
-
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("");
+
+  const handleChange = (e) => {
+    setColor(e.target.value);
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onChange={handleChange} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
