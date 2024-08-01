39020032e23fbb345262aabb4a8d1dad7489d942
byeolee1221 <zicsso1221@gmail.com>
2024-08-01 15:49:37 +0900

    feat: 색상 입력하면 자동으로 바뀌게 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import ColorBox from "./ColorBox";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
+
+  const onChnageValue = (e) => {
+    setColor(e.target.value);
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onChange={onChnageValue} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
