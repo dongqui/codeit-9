550f78c4d2526891b1e18e35c0386251ff204a1e
DrowsyzzZ <daenamu222@naver.com>
2024-08-01 15:57:28 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import ColorBox from "./ColorBox";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
+  const saveColorValue = (event) => {
+    setColor(event.target.value);
+  };
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input type="text" onChange={saveColorValue} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
