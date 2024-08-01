487bc375c2ef0ed1f661ca386a8d107eee40000e
mungyun <mungyun1234@naver.com>
2024-08-01 16:14:57 +0900

    Add setInputValue

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,21 +1,30 @@
 import ColorBox from "./ColorBox";
 import React, { useState } from "react";
 import "./App.css";
+import ResetButton from "./ResetButton";
 
 function App() {
   const [color, setColor] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
   const handleChange = (e) => {
     setColor(e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  const handleReset = () => {
+    setColor("");
+    setInputValue("");
   };
 
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input onChange={handleChange} />
+        <input value={inputValue} onChange={handleChange} />
       </div>
       <ColorBox color={color} />
+      <ResetButton onClick={handleReset} />
     </div>
   );
 }
