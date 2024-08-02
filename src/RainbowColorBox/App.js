e2073d63ba54fc0c1a1b290ac3dad63f79c14a15
jihyemin <hyemeeeny@gamil.com>
2024-08-02 11:42:33 +0900

    실습추가

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -3,11 +3,18 @@ import ColorBox from "./ColorBox";
 import "./App.css";
 
 function App() {
+  const inputTextColor = color => {
+    if (input.value) {
+      ColorBox;
+    } else if (input.value === '') {
+      ColorBox;
+    }
+  };
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onInput={inputTextColor} />
       </div>
       <ColorBox />
     </div>
