818d192990f430deed3be9564e255802a42d0dbd
JeongminSeon <skelters@naver.com>
2024-08-02 11:33:27 +0900

    Finish

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,22 @@
 import ColorBox from "./ColorBox";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState();
+
+  function handleColor(event) {
+    setColor(event.target.value);
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onChange={handleColor} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
