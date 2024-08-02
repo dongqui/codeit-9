6106366fe355c5332ff498e9ed23b1d6f6144ec1
Wooy <wjddndud5023@gmail.com>
2024-08-02 11:38:17 +0900

    실습

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import ColorBox from "./ColorBox";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("white");
+
+  function colorChange() {
+    const inputValue = document.querySelector(".colorInput");
+    setColor(inputValue);
+  }
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input className="colorInput" onChange={colorChange} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
