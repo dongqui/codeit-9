250875d1b256616425d3441ebc293bb369b59b27
Wooy <wjddndud5023@gmail.com>
2024-08-06 11:40:59 +0900

    실습2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import "./App.css";
 import { useState } from "react";
 import ResetButton from "./ResetButton";
 
+const RAINBOW_COLORS = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "navy",
+  "purple",
+];
+
 function App() {
   const [color, setColor] = useState("white");
 
   function colorChange(e) {
     setColor(e.currentTarget.value);
+    if (!RAINBOW_COLORS.includes(e.currentTarget.value)) {
+      const message = document.querySelector(".message");
+      message.innerHTML = "무지개색이 아닙니다.";
+    } else {
+      const message = document.querySelector(".message");
+      message.innerHTML = "";
+    }
+  }
+
+  function handleClickReset() {
+    setColor("");
   }
 
   return (
@@ -18,13 +39,8 @@ function App() {
         <input onChange={colorChange} />
       </div>
       <ColorBox color={color} />
-      <button>
-        <ResetButton
-          onClick={(value) => {
-            setColor(value);
-          }}
-        />
-      </button>
+      <p className="message"></p>
+      <ResetButton onClick={handleClickReset} />
     </div>
   );
 }
