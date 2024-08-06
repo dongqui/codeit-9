a21e89ca279a2e4992723ffc45876b5070e4bb38
Dongjin <dongqui9@gmail.com>
2024-08-06 18:29:51 +0900

    init todo

---
+++ src/RainbowColorBox/ResetButton.jsx
@@ -0,0 +1,3 @@
+export default function ResetButton({ onClick }) {
+  return <button onClick={onClick}>초기화</button>;
+}
--- src/ResetButton.jsx
+++
@@ -1,3 +0,0 @@
-export default function ResetButton({ onClick }) {
-  return <button onClick={onClick}>초기화</button>;
-}
