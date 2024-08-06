25af34835613b1b00446e98fa7c1b04d26241921
sxyoung <010kate@naver.com>
2024-08-06 11:17:05 +0900

    두 번째

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,23 +1,29 @@
 import ColorBox from "./ColorBox";
+import ResetButton from "./ResetButton";
 
 import "./App.css";
 
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState("");
 
-  const inputColor = e => {
+  const inputColor = (e) => {
     setColor(e.currentTarget.value);
+  };
+
+  const clearColor = () => {
+    setColor("");
   };
 
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input onChange={inputColor}/>
+        <input onChange={inputColor} />
       </div>
-      <ColorBox color = {color} />
+      <ColorBox color={color} />
+      <ResetButton onClick={clearColor} />
     </div>
   );
 }
---
+++ src/RainbowColorBox/ResetButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import App from "./App";
+
+const ResetButton = ({ onClick }) => {
+  return <button onClick={onClick}>초기화</button>;
+};
+
+export default ResetButton;
