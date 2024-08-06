e21caf2a6e5553f19f3c05427499c866aabf2dde
junjeeong <wn8624@naver.com>
2024-08-06 14:10:39 +0900

    정준영

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,26 @@
 import ColorBox from "./ColorBox";
-
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState('white');
+
+  const changeColor = (e) => {
+    setColor(e.target.value);
+  }
+
+  function resetColor() {
+    setColor('white');
+  }
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onChange={changeColor} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
+      <button onClick={resetColor}>초기화</button>
     </div>
   );
 }
