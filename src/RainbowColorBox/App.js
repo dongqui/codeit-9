4aacd123e79881762cf055b6736a2ffad476ea10
yyezzzy <t47047877@gmail.com>
2024-08-06 14:47:18 +0900

    ⚡️ 무지개색 판별

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,18 +1,29 @@
-import ColorBox from './ColorBox';
 import './App.css';
 import { useState } from 'react';
+import ColorBox from './ColorBox';
 import { ResetButton } from './ResetButton';
 
 function App() {
   const [color, setColor] = useState('white');
+  const [error, setError] = useState('');
 
   const RAINBOW_COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'purple'];
 
   // 상태값 변경 함수
   const onChange = (e) => {
-    setColor(e.target.value);
+    const newColor = e.target.value;
+    if (RAINBOW_COLORS.includes(newColor)) {
+      setColor(newColor);
+      setError('');
+    } else {
+      setError('Error: 색상이 유효하지 않습니다.');
+    }
   };
-  const handleResetClick = () => setColor('white');
+
+  const handleResetClick = () => {
+    setColor('white');
+    setError('');
+  };
 
   return (
     <div className="App">
@@ -22,9 +33,7 @@ function App() {
       </div>
       <ColorBox color={color} />
       <ResetButton onClick={handleResetClick} />
-      {RAINBOW_COLORS.map((color) => (
-        <div>{color}</div>
-      ))}
+      {error && <div className="error">{error}</div>}
     </div>
   );
 }
