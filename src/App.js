19f245b554bdbd47f051aa008a2b4ff8ba969435
Wooy <wjddndud5023@gmail.com>
2024-08-06 11:15:55 +0900

    실습

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import ColorBox from "./ColorBox";
 
 import "./App.css";
 import { useState } from "react";
+import ResetButton from "./ResetButton";
 
 function App() {
   const [color, setColor] = useState("white");
 
-  function colorChange() {
-    const inputValue = document.querySelector(".colorInput");
-    setColor(inputValue.value);
+  function colorChange(e) {
+    setColor(e.currentTarget.value);
   }
 
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input className="colorInput" onChange={colorChange} />
+        <input onChange={colorChange} />
       </div>
       <ColorBox color={color} />
+      <button>
+        <ResetButton
+          onClick={(value) => {
+            setColor(value);
+          }}
+        />
+      </button>
     </div>
   );
 }
