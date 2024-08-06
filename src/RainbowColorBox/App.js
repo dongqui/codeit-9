904cf872c4efbde86167c42b70aa6d26c82e23fe
pjy2466 <pjy2466@naver.com>
2024-08-06 14:18:16 +0900

    초기화버튼추가

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,28 @@
 import ColorBox from "./ColorBox";
-
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("white");
+
+  // 상태값 변경 함수
+  const onChange = (e) => {
+    setColor(e.target.value);
+  };
+
+  // 초기화 함수
+  const resetColor = () => {
+    setColor("white");
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input/>
+        <input onChange={onChange} />
+        <button onClick={resetColor}>초기화</button>
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
