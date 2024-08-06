57f4e8ce9fc5e6479c60ab806586833b4e15ac8d
cccwon2 <cccwon2@naver.com>
2024-08-06 14:51:46 +0900

    에러 박스 코드 개선

--- src/ErrorBox.jsx
+++ src/ErrorBox.jsx
@@ -1,7 +1,6 @@
 const COLORS = ["red", "orange", "yellow", "green", "blue", "navy", "purple"];
 
 export default function ErrorBox({ color }) {
-  console.log(color);
-  if (!COLORS.includes(color)) return <>무지개 색이 아닌데요!!??</>;
-  else return "";
+  //console.log(color);
+  return <>{!COLORS.includes(color) && "무지개 색이 아닌데요!!??"}</>;
 }
