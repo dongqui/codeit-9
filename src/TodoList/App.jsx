f481fcee760cc63f24bd6ce0de937e46bf23055b
Dongjin <dongqui9@gmail.com>
2024-08-07 11:04:44 +0900

    Todo 3: 할 일 수정

--- src/TodoList/App.jsx
+++ src/TodoList/App.jsx
@@ -6,20 +6,25 @@ import TodoItem from "./TodoItem";
 export default function App() {
   const [todoList, setTodoList] = useState([]);
 
-  const handleAddClick = (todo) => setTodoList([todo, ...todoList]);
-  const handleDeleteClick = (id) =>
+  const addTodo = (newTodo) => setTodoList([newTodo, ...todoList]);
+  const deleteTodo = (id) =>
     setTodoList(todoList.filter((todo) => todo.id !== id));
-
+  const editTodo = (editedTodo) => {
+    setTodoList(
+      todoList.map((todo) => (todo.id === editedTodo.id ? editedTodo : todo))
+    );
+  };
   return (
     <div>
-      <TodoInput onAddClick={handleAddClick} />
+      <TodoInput onAddClick={addTodo} />
       <ul>
         {todoList.map((todo) => (
           <TodoItem
             key={todo.id}
             id={todo.id}
             title={todo.title}
-            onDeleteClick={handleDeleteClick}
+            onDeleteClick={deleteTodo}
+            onEditConfirmClick={editTodo}
           />
         ))}
       </ul>
--- src/TodoList/TodoItem.jsx
+++ src/TodoList/TodoItem.jsx
@@ -1,11 +1,42 @@
-export default function TodoItem({ id, title, onDeleteClick }) {
-  return (
+import { useState } from "react";
+
+export default function TodoItem({
+  id,
+  title,
+  onDeleteClick,
+  onEditConfirmClick,
+}) {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editValue, setEditValue] = useState(title);
+
+  const handleInputChange = (e) => setEditValue(e.currentTarget.value);
+  const handleEditClick = () => setIsEdit(true);
+  const handleCancelClick = () => setIsEdit(false);
+  const handleDeleteClick = () => onDeleteClick(id);
+  const handleEidtConfirmClick = () => {
+    onEditConfirmClick({ id, title: editValue });
+    setIsEdit(false);
+  };
+
+  return isEdit ? (
+    <li>
+      <input value={editValue} onChange={handleInputChange} />
+      <button type="button" onClick={handleEidtConfirmClick}>
+        확인
+      </button>
+      <button type="button" onClick={handleCancelClick}>
+        취소
+      </button>
+    </li>
+  ) : (
     <li>
       {title}
-      <button type="button" onClick={() => onDeleteClick(id)}>
+      <button type="button" onClick={handleDeleteClick}>
         삭제
       </button>
-      <button type="button">수정</button>
+      <button type="button" onClick={handleEditClick}>
+        수정
+      </button>
     </li>
   );
 }
