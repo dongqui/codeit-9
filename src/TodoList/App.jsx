b69ebe8b350810243708127a28b07d63ef883b4d
Dongjin <dongqui9@gmail.com>
2024-08-07 10:42:26 +0900

    Todo 2: 할 일 삭제

--- src/TodoList/App.jsx
+++ src/TodoList/App.jsx
@@ -7,13 +7,20 @@ export default function App() {
   const [todoList, setTodoList] = useState([]);
 
   const handleAddClick = (todo) => setTodoList([todo, ...todoList]);
+  const handleDeleteClick = (id) =>
+    setTodoList(todoList.filter((todo) => todo.id !== id));
 
   return (
     <div>
       <TodoInput onAddClick={handleAddClick} />
       <ul>
         {todoList.map((todo) => (
-          <TodoItem key={todo.id} title={todo.title} />
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            onDeleteClick={handleDeleteClick}
+          />
         ))}
       </ul>
     </div>
--- src/TodoList/TodoItem.jsx
+++ src/TodoList/TodoItem.jsx
@@ -1,8 +1,10 @@
-export default function TodoItem({ title }) {
+export default function TodoItem({ id, title, onDeleteClick }) {
   return (
     <li>
       {title}
-      <button type="button">삭제</button>
+      <button type="button" onClick={() => onDeleteClick(id)}>
+        삭제
+      </button>
       <button type="button">수정</button>
     </li>
   );
