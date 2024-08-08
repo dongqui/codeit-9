d0afaaa5bb5319442935bd5c43f6f9aa2dba05f9
kim-miso <smilekim94@gmail.com>
2024-08-08 14:40:27 +0900

    인풋만들기

--- src/TodoList/App.jsx
+++ src/TodoList/App.jsx
@@ -2,11 +2,17 @@ import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 
 export default function App() {
+	const [items, setItems] = useState([]);
+
+	const handleCreate = (todo) => {
+		setItems((prevItems) => [todo, ...prevItems]);
+	}
+
   return (
     <div>
       <TodoInput />
       <ul>
-        <TodoItem />
+        <TodoItem submitSuccess = { handleCreate[setItems].map }/>
       </ul>
     </div>
   );
--- src/TodoList/TodoInput.jsx
+++ src/TodoList/TodoInput.jsx
@@ -1,9 +1,23 @@
+//임시코드
+
+import { useState } from "react";
+
+//리액트 배열 사용할 때 key값 줘야 해서 만든 코드
 let id = 0;
 
+//id를 사용해서 하나씩 id에 추가되는 형식으로 작성
+
 export default function TodoInput() {
+	const [todo, setTodo] = useState("");
+
+	const addTodo = () => {
+		setTodo([...todo, {id: id++, content: todo}]);
+		setTodo("");
+	}
+
   return (
     <>
-      <input />
+      <input value={todo} />
       <button>입력</button>
     </>
   );
