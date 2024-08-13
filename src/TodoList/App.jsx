f4267e3abe4eb1bfc487698b9da9ebf55dcd23cb
mungyun <mungyun1234@naver.com>
2024-08-13 16:18:04 +0900

    Add Edit functions s

--- src/TodoList/App.jsx
+++ src/TodoList/App.jsx
@@ -6,6 +6,8 @@ let nextId = 1;
 
 export default function App() {
   const [todos, setTodos] = useState([]);
+  const [editingId, setEditingId] = useState(null);
+  const [editingText, setEditingText] = useState("");
 
   const addTodo = (text) => {
     setTodos([...todos, { id: nextId, text }]);
@@ -16,7 +18,25 @@ export default function App() {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-  const editTodo = (id) => {};
+  const editTodo = (id, text) => {
+    setEditingId(id);
+    setEditingText(text);
+  };
+
+  const saveEditTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, text: editingText } : todo
+      )
+    );
+    setEditingId(null);
+    setEditingText("");
+  };
+
+  const cancelEditTodo = () => {
+    setEditingId(null);
+    setEditingText("");
+  };
 
   return (
     <div>
@@ -29,6 +49,11 @@ export default function App() {
             text={todo.text}
             onDelete={deleteTodo}
             onEdit={editTodo}
+            onSaveEdit={saveEditTodo}
+            onCancelEdit={cancelEditTodo}
+            isEditing={editingId === todo.id}
+            editingText={editingText}
+            setEditingText={setEditingText}
           />
         ))}
       </ul>
--- src/TodoList/TodoItem.jsx
+++ src/TodoList/TodoItem.jsx
@@ -1,13 +1,35 @@
-export default function TodoItem({ id, text, onDelete, onEdit }) {
+import React from "react";
+
+export default function TodoItem({
+  id,
+  text,
+  onDelete,
+  onEdit,
+  onSaveEdit,
+  onCancelEdit,
+  isEditing,
+  editingText,
+  setEditingText,
+}) {
   return (
     <li>
-      {text}
-      <button type="button" onClick={() => onDelete(id)}>
-        삭제
-      </button>
-      <button type="button" onClick={() => onEdit(id)}>
-        수정
-      </button>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={editingText}
+            onChange={(e) => setEditingText(e.target.value)}
+          />
+          <button onClick={() => onSaveEdit(id)}>확인</button>
+          <button onClick={onCancelEdit}>취소</button>
+        </>
+      ) : (
+        <>
+          <span>{text}</span>
+          <button onClick={() => onEdit(id, text)}>수정</button>
+          <button onClick={() => onDelete(id)}>삭제</button>
+        </>
+      )}
     </li>
   );
 }
