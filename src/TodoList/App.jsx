de7e9d028811559973a68691d2c7e93448290948
sxyoung <010kate@naver.com>
2024-08-16 11:41:09 +0900

    서버

--- src/TodoList/App.jsx
+++ src/TodoList/App.jsx
@@ -6,45 +6,44 @@ import TodoItem from "./TodoItem";
 export default function App() {
   const [todoList, setTodoList] = useState([]);
 
-  const getData = async () => {
-    const data = await getTodo();
-    setTodoList(data);
-
-    useEffect(() => {
-      getData();
-    }, []);
-
-    const handleOnClick = (newTodo) => {
-      setTodoList([...todoList, newTodo]);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getTodo();
+      setTodoList(data);
     };
+    getData();
+  }, []);
 
-    const handleDelete = (id) => {
-      const nextTodoList = todoList.filter((item) => item.id !== id);
-      setTodoList(nextTodoList);
-    };
-
-    const handleEdit = (id, newValue) => {
-      setTodoList((prevTodoList) => {
-        prevTodoList.map((item) =>
-          item.id === id ? { ...item, value: newValue } : item
-        );
-      });
-    };
-
-    return (
-      <div>
-        <TodoInput onSubmit={handleOnClick} />
-        <ul>
-          {todoList.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              value={todo.value}
-              id={todo.id}
-              onDelete={handleDelete}
-            />
-          ))}
-        </ul>
-      </div>
-    );
+  const handleOnClick = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
   };
+
+  const handleDelete = (id) => {
+    const nextTodoList = todoList.filter((item) => item.id !== id);
+    setTodoList(nextTodoList);
+  };
+
+  const handleEdit = (id, newValue) => {
+    setTodoList((prevTodoList) => {
+      prevTodoList.map((item) =>
+        item.id === id ? { ...item, value: newValue } : item
+      );
+    });
+  };
+
+  return (
+    <div>
+      <TodoInput onSubmit={handleOnClick} />
+      <ul>
+        {todoList.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            value={todo.value}
+            id={todo.id}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
