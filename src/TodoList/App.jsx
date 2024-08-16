6f1ba757ee69cea206537742bab6a82980b96576
byeolee1221 <zicsso1221@gmail.com>
2024-08-16 16:24:57 +0900

    feat: 서버에서 데이터 가져오기 구현

--- src/TodoList/App.jsx
+++ src/TodoList/App.jsx
@@ -1,23 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 
 export default function App() {
   const [toDoList, setToDoList] = useState([]);
-
-  const onSubmit = (id, value) => {
-    setToDoList((prevToDo) => [...prevToDo, { id: id++, value }]);
+  
+  const onSubmit = (id, title) => {
+    setToDoList((prevToDo) => [...prevToDo, { id: id++, title }]);
   };
 
   const onDeleteToDoList = (id) => {
     setToDoList((prevList) => prevList.filter((item) => item.id !== id));
   };
 
-  const onEditToDoList = (id, newValue) => {
+  const onEditToDoList = (id, newTitle) => {
     setToDoList((prevList) =>
-      prevList.map((item) => (item.id === id ? { ...item, value: newValue } : item))
+      prevList.map((item) => (item.id === id ? { ...item, title: newTitle } : item))
     );
   };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch("/todos");
+
+        const data = await response.json();
+        setToDoList(data);
+        
+      } catch (error) {
+        console.error("getDate에서 오류 발생", error);
+      }
+    }
+
+    getData();
+  }, []);
 
   return (
     <div>
--- src/TodoList/TodoItem.jsx
+++ src/TodoList/TodoItem.jsx
@@ -2,20 +2,20 @@ import { useState } from "react";
 
 export default function TodoItem({ id, toDo, onDeleteToDoList, onEditToDoList }) {
   const [isEdit, setEdit] = useState(false);
-  const [newValue, setNewValue] = useState(toDo.value);
+  const [newTitle, setNewTitle] = useState(toDo.title);
 
   const onDelete = () => {
     onDeleteToDoList(id);
   };
 
-  const onChangeNewValue = (e) => {
+  const onChangeNewTitle = (e) => {
     const userInput = e.target.value;
-    setNewValue(userInput);
+    setNewTitle(userInput);
   };
 
   const onEdit = (e) => {
     e.preventDefault();
-    onEditToDoList(id, newValue);
+    onEditToDoList(id, newTitle);
     setEdit(false);
   };
 
@@ -27,13 +27,13 @@ export default function TodoItem({ id, toDo, onDeleteToDoList, onEditToDoList })
     <li>
       {isEdit ? (
         <form onSubmit={onEdit}>
-          <input type="text" value={newValue} name="newValue" onChange={onChangeNewValue} />
+          <input type="text" value={newTitle} name="newTitle" onChange={onChangeNewTitle} />
           <button type="submit">저장</button>
           <button onClick={onCancel}>취소</button>
         </form>
       ) : (
         <>
-          <span>{toDo.value}</span>
+          <span>{toDo.title}</span>
           <button type="button" onClick={onDelete}>
             삭제
           </button>
