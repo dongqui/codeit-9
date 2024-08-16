d58e388da3ead86f3c4d9a934d35a24a11e2ea38
jihyemin <hyemeeeny@gamil.com>
2024-08-16 11:35:37 +0900

    api 추가

--- src/TodoList/App.jsx
+++ src/TodoList/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
+import { getTodoList } from '../api.js';
 
 export default function App() {
   const [todoList, setTodoList] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/todos');
+      const data = await response.json();
+      setTodoList(data);
+    } catch (error) {
+      console.error('데이터를 불러오는데 실패하였습니다.');
+    }
+  };
+  fetchData();
 
   const addTodo = todo => {
     setTodoList([...todoList, todo]);
